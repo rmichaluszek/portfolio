899d3be92342bb07edf90a5dd079ffad147374ad
Vunnysher <vunnysher@gmail.com>
2019-04-24 01:19:45 +0100

    Added language switcher to topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -16,15 +16,15 @@ class Topbar extends Component {
       <div className="Topbar">
         <div className="Container">
           <Logo/>
+          <select value={this.props.language} onChange={this.onLanguageChange}>
+            <option value="en">English</option>
+            <option value="pl">Polski</option>
+          </select>
           <div className="Links">
             <Translate content="topbar.title" component="a" href="#"/>
             <Translate content="topbar.mywork" component="a" href="#mywork"/>
             <Translate content="topbar.contact" component="a" href="#contact"/>
           </div>
-          <select value={this.props.language} onChange={this.onLanguageChange}>
-            <option value="en">English</option>
-            <option value="pl">Polski</option>
-          </select>
         </div>
       </div>
     );
