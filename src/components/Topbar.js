c50f7c6caeec1244b3ebd3d7d72f5377f42e7c19
Vunnysher <vunnysher@gmail.com>
2019-04-25 13:57:42 +0100

    Added topbar responsivity

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -7,8 +7,17 @@ import Logo from './Logo';
 
 class Topbar extends Component {
 
+  state = {
+      mobileMenuToggled: false
+  }
+
   onLanguageChange = (e) => {
       this.props.changeLanguage(e.target.value);
+  }
+
+  toggleMobileMenu = (e) => {
+      var isMenuShown = !this.state.mobileMenuToggled;
+      this.setState({'mobileMenuToggled':isMenuShown});
   }
 
   render() {
@@ -16,14 +25,15 @@ class Topbar extends Component {
       <div className="Topbar">
         <div className="Container">
           <Logo/>
-          <select value={this.props.language} onChange={this.onLanguageChange}>
-            <option value="en">English</option>
-            <option value="pl">Polski</option>
-          </select>
-          <div className="Links">
+          <div onClick={this.toggleMobileMenu} className={this.state.mobileMenuToggled ? 'MenuButton MenuButtonActive': 'MenuButton'}>&#9776;</div>
+          <div className={this.state.mobileMenuToggled ? 'Links MenuShown': 'Links'}>
             <Translate content="topbar.title" component="a" href="#"/>
             <Translate content="topbar.mywork" component="a" href="#mywork"/>
             <Translate content="topbar.contact" component="a" href="#contact"/>
+            <select value={this.props.language} onChange={this.onLanguageChange}>
+              <option value="en">English</option>
+              <option value="pl">Polski</option>
+            </select>
           </div>
         </div>
       </div>
