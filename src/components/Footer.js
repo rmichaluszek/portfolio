c8176a84b4466659f42b4724b1acfd7e01094a5f
Vunnysher <vunnysher@gmail.com>
2019-04-30 19:15:28 +0100

    Fixed links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,9 @@ class Footer extends Component {
       <div className="Footer">
         <div className="Container">
           <div className="Socials">
-            <a href="" alt="facebook"><button><img src="/images/facebook.svg"></img></button></a>
-            <a href="" alt="facebook"><button><img src="/images/twitter.svg"></img></button></a>
-            <a href="" alt="facebook"><button><img src="/images/github.svg"></img></button></a>
+            <a href="https://www.facebook.com/rafalm99" alt="facebook"><button><img src="/images/facebook.svg"></img></button></a>
+            <a href="https://twitter.com/rafalm99" alt="twitter"><button><img src="/images/twitter.svg"></img></button></a>
+            <a href="https://github.com/rafalm99" alt="github"><button><img src="/images/github.svg"></img></button></a>
           </div>
           <div className="Username">/rafalm99</div>
           <Translate component="div" className="FooterCopyright" content="footer.copyright"/>
